f6585aca8315f9bdb1ecf413240568f1274def16
Stephen <stevedipietro97@gmail.com>
2023-08-08 11:53:25 -0400

    change dependencies

---
+++ ShoppingListReactNative/ShoppingListReactNative/ShoppingListReactNativeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListReactNativeApp.swift
+//  ShoppingListReactNative
+//
+//  Created by Stephen DiPietro on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListReactNativeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
